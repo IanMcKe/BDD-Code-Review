2b9bd0add6802a9a6b84eaf695ecffad16c8d801
Ian McKenney <ian.jafool@gmail.com>
2015-08-14 10:18:33 -0700

    add tests for 2-4 specs

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -17,5 +17,49 @@
             //Assert
             $this->assertEquals(1, $result);
         }
+
+        function test_oneLetterOneWord()
+        {
+            //Arrange
+            $test_repeatCounter = new RepeatCounter;
+            $input_word = "a";
+            $input_string = "abacus";
+
+            //Act
+            $result = $test_repeatCounter->countRepeats($input_word, $input_string);
+
+            //Assert
+            $this->assertEquals(0, $result);
+        }
+
+        function test_OneWordEverything()
+        {
+            //Arrange
+            $test_repeatCounter = new RepeatCounter;
+            $input_word = "act";
+            $input_string = "act";
+
+            //Act
+            $result = $test_repeatCounter->countRepeats($input_word, $input_string);
+
+            //Assert
+            $this->assertEquals(1, $result);
+        }
+
+        function test_multipleWordsNoSubstring()
+        {
+            //Arrange
+            $test_repeatCounter = new RepeatCounter;
+            $input_word = "act";
+            $input_string = "I act well";
+
+            //Act
+            $result = $test_repeatCounter->countRepeats($input_word, $input_string);
+
+            //Assert
+            $this->assertEquals(1, $result);
+        }
+
+
     }
 ?>
