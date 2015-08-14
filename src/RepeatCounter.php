57d948d8c35a715eac5525e4fad04d7f41a8d300
Ian McKenney <ian.jafool@gmail.com>
2015-08-14 11:11:00 -0700

    pass 9th spec

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -5,11 +5,38 @@
         {
             $input_check = explode(" ", $user_word);
             if(count($input_check) == 1){
-                $lower_case_word = strtolower($user_word);
-                $string_array = explode(" ", strtolower($user_string));
-                $result = 0;
+                //remove punctuation from $user_word
+                $user_word_chars = str_split($user_word);
+                $punctuation = array("?",".",",","!",":",";","(",")","[","]","{","}","/","'",'"',"*","$","#");
+                foreach($user_word_chars as $character){
+                    if(in_array($character, $punctuation)){
+                        $punctuation_char_key = array_search($character, $user_word_chars);
+                        unset($user_word_chars[$punctuation_char_key]);
+                    }
+                }
+                $formatted_word = implode($user_word_chars);
+
+                //remove punctuation from $user_string and push results into new array
+                $string_array = explode(" ", $user_string);
+                $formatted_string = array();
                 foreach($string_array as $word){
-                    if ($lower_case_word == $word){
+                    $user_string_chars = str_split($word);
+                    foreach($user_string_chars as $character){
+                        if(in_array($character, $punctuation)){
+                            $punctuation_char_key = array_search($character, $user_string_chars);
+                            unset($user_string_chars[$punctuation_char_key]);
+                        }
+                    }
+                    $formatted_part = implode($user_string_chars);
+                    array_push($formatted_string, strtolower($formatted_part));
+                }
+
+                //lower case strings and count matches
+                $lower_case_word = strtolower($formatted_word);
+
+                $result = 0;
+                foreach($formatted_string as $match){
+                    if ($lower_case_word == $match){
                         ++$result;
                     }
                 }
